3e15dc7142cce1b4c037d0959b0d29f5cc94a9fd
Mabrikos <Steamgeneral@yandex.ru>
2018-04-15 11:25:58 +0700

    Geting source code

--- Webdriver.py
+++ Webdriver.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
+import pickle
 
 usr = input('Enter username: ')
 pwd = input('Enter password: ')
@@ -25,9 +28,13 @@ driver.get('https://steamcommunity.com/market/search?appid=570#p180_price_asc')
 # for i in range 10:
 # driver.find_element_by_xpath('//*[@id="result_{}"]/div[2]'.format(i)).send_keys(Keys.CONTROL + 't')
 lot_wait = WebDriverWait(driver, 10)
-lot_wait.until(EC.visibility_of_element_located(
+lot_wait.until(EC.presence_of_element_located(
     (By.XPATH, '//*[@id="result_0"]/div[2]')))
 driver.find_element_by_xpath(
-    '//*[@id="result_0"]/div[2]').send_keys(Keys.CONTROL + 't')
+    '//*[@id="result_0"]/div[2]').click()  # send_keys(Keys.CONTROL + 't')
+# Sourcecode scraping
+with open("file.txt", 'w', encoding='utf-8') as f:
+    f.write(driver.page_source)
+f.close()
 input()
 driver.close()
