7cca30ac3e7ce5439f1e1cc5510ed8a143cf63a6
Mabrikos <Steamgeneral@yandex.ru>
2018-08-26 07:19:32 +0700

    Removed something useless

--- Webdriver.py
+++ Webdriver.py
@@ -7,11 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 import time
 import re
 import sys
-import os
 
 
 def ReductionInit():
